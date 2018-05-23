0d140fb1bf6c05e2b702e597ff401d1982ac7f3b
mbalazs9001 <mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.com>
2018-05-23 11:02:55 +0200

    Added Product model

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,8 @@
+export class Product {
+  constructor(public defaultPrice: number,
+              public defaultCurrency: string,
+              public imageName: string,
+              public productCategoryId: number,
+              public supplierId: number
+              ) {}
+}
