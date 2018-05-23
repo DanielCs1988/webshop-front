fe56682bef560980638ea0690f5a590541a99424
Adam Feher <fhradm@gmail.com>
2018-05-23 10:03:35 +0200

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+import { AddressComponent } from './address/address.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { PaymentComponent } from './payment/payment.component';
+import { PaypalComponent } from './payment/paypal/paypal.component';
+import { CreditCardComponent } from './payment/credit-card/credit-card.component';
+import { MenuComponent } from './navbar/menu/menu.component';
+import { ShoppingCartComponent } from './navbar/shopping-cart/shopping-cart.component';
+import { LoginComponent } from './navbar/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RegisterComponent,
+    AddressComponent,
+    ProductListComponent,
+    PaymentComponent,
+    PaypalComponent,
+    CreditCardComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
