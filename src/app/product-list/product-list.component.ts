2ebe40ed8f014f2249db57c5247ed206829daf60
DanielCs88 <daniel.cs20@gmail.com>
2018-05-24 15:47:20 +0200

    Product list can receive filters via queryparams.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Product} from '../models/product.model';
 import {ProductListService} from '../services/product-list.service';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -11,13 +12,21 @@ import {ProductListService} from '../services/product-list.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
+  categoryFilter: number = null;
+  supplierFilter: number = null;
 
-  constructor(public productListService: ProductListService) { }
+  constructor(public productListService: ProductListService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.productListService.getProducts().subscribe(
       products => this.products = products
-    )
+    );
+    this.route.queryParams.subscribe(
+      (params: Params) => {
+        this.categoryFilter = +params['category'];
+        this.supplierFilter = +params['supplier'];
+      }
+    );
   }
 
 }
