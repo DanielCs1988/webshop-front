d0d4988a4c16d37b306bb9ee3d50eef393fdc8e1
DanielCs88 <daniel.cs20@gmail.com>
2018-05-24 13:19:28 +0200

    Proxy works

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -9,7 +9,7 @@ export class AuthInterceptorService implements HttpInterceptor {
   constructor(private authService: AuthService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.authService.isLoggedIn() ? this.authService.user.token : null;
+    const token = this.authService.isLoggedIn() ? this.authService.user.token : '';
     const reqWithAuth = req.clone({headers: req.headers.append('Authorization', token)});
     return next.handle(reqWithAuth);
   }
